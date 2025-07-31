7715d1dc1f88cb5e87dbc10663e7c934fe6deaf9
Codú <134159765+BrayanBCode@users.noreply.github.com>
2025-07-30 22:41:50 -0300

    si

--- Player Stuff/Player.cs
+++ Player Stuff/Player.cs
@@ -10,12 +10,17 @@ public partial class Player : RigidBody3D
 
     private bool isJumping = false;
     private bool isGrounded = false;
-    private Vector3 move = Vector3.Zero;
+    private Vector3 lastKnownMousePosition = Vector3.Zero;
+    private Vector3 move;
 
     //Events keyboard
     public override void _Input(InputEvent @event)
     {
         base._Input(@event);
+
+        if (@event.IsActionPressed("LMB") && isGrounded) OnClick();
+
+        move = Vector3.Zero;
 
         move.X = Input.GetAxis("ui_left", "ui_right");
         move.Z = Input.GetAxis("ui_up", "ui_down");
@@ -29,35 +34,14 @@ public partial class Player : RigidBody3D
         ApplyGravity(delta);
     }
 
-
-    public void OnClick(Node camera, InputEvent inputEvent, Vector3 event_position, Vector3 normal, int shapeIDX)
+    public void MouseInput(Node camera, InputEvent inputEvent, Vector3 event_position, Vector3 normal, int shapeIDX)
     {
-
-        JumpImpulse(inputEvent, event_position);
+        lastKnownMousePosition = event_position;
     }
 
-
-    /// <summary>
-    /// Applies a jump impulse to the player when the left mouse button is pressed.
-    /// The player will only jump if they are currently on the ground (LinearVelocity.Y is approximately 0).
-    /// The jump direction is determined by the position of the mouse click.
-    /// The horizontal jump strength is applied in the direction of the click, and
-    /// the vertical jump strength is applied upwards.
-    /// </summary>
-    /// <remarks>
-    /// This method checks if the left mouse button is pressed and if the player is on the ground (LinearVelocity.Y is approximately 0).
-    /// If both conditions are met, it calculates the direction from the player's position to the event position,
-    /// applies a force in that direction multiplied by the horizontal jump strength,
-    /// and sets the vertical component of the force to the vertical jump strength.
-    /// The resulting force is then applied to the player's rigid body to create the jump effect.
-    /// </remarks>
-    /// <param name="inputEvent">The input event that triggered the jump.</param>
-    /// <param name="event_position">The position in the world where the mouse click occurred.</param>
-    public void JumpImpulse(InputEvent inputEvent, Vector3 event_position)
+    public void OnClick()
     {
-        if (!inputEvent.IsActionPressed("LMB") || !Mathf.IsEqualApprox(LinearVelocity.Y, 0)) return;
-
-        Vector3 dir = Position.DirectionTo(event_position);
+        Vector3 dir = Position.DirectionTo(lastKnownMousePosition);
         Vector3 push = Utilities.MultipyVector(dir, horizontalJumpStrength);
         push.Y = verticalJumpStrength;
 
@@ -72,7 +56,7 @@ public partial class Player : RigidBody3D
         if (isGrounded)
         {
             Vector3 mov = move * speed * (float)delta;
-            ApplyCentralForce(mov);
+            ApplyForce(mov);
         }
 
         // Revisar si está en el piso
